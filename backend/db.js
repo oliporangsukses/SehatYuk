6a5f1e26a07a40f5bd1f51ed0a8e8fd2822e4617
Rasmiati <242510203@smkn1padaherang.sch.id>
2026-04-08 19:25:32 +0700

    update db js

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,17 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "sehatyuk",
-  port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("❌ Koneksi gagal:", err);
+    console.error("❌ Koneksi database gagal:");
+    console.error(err);
   } else {
     console.log("✅ Koneksi database berhasil!");
   }
